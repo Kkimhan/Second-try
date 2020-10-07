d81f71084de7d5a5522567d25876415300978c2d
Kkimhan <72479567+Kkimhan@users.noreply.github.com>
2020-10-07 13:47:20 -1000

    Update four.php

--- four.php
+++ four.php
@@ -4,7 +4,7 @@
             $target_path = DVWA_WEB_PAGE_TO_ROOT."hackable/uploads/"; 
             $target_path = basename( $_FILES['uploaded']['name']); 
 
-            if(!move_uploaded_file($_FILES['uploaded']['tmp_name'], $target_path)) { 
+            if (!move_uploaded_file($_FILES['uploaded']['tmp_name'], $target_path)) { 
                  
               $html  .=   echo '<pre>'; 
                 echo 'Your image was not uploaded.'; 
