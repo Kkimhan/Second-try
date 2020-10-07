be754b343f56c036589c48c6db2d6303e1b51230
Kkimhan <72479567+Kkimhan@users.noreply.github.com>
2020-10-06 22:59:21 -1000

    Create four.php

---
+++ four.php
@@ -0,0 +1,22 @@
+<?php 
+    if (isset($_POST['Upload'])) { 
+
+            $target_path = DVWA_WEB_PAGE_TO_ROOT."hackable/uploads/"; 
+            $target_path = basename( $_FILES['uploaded']['name']); 
+
+            if(!move_uploaded_file($_FILES['uploaded']['tmp_name'], $target_path)) { 
+                 
+              $html  .=   echo '<pre>'; 
+                echo 'Your image was not uploaded.'; 
+                echo '</pre>'; 
+                 
+              } else { 
+             
+             $html .=    echo '<pre>'; 
+                echo $target_path . ' succesfully uploaded!'; 
+                echo '</pre>'; 
+                 
+            } 
+
+        } 
+?>
